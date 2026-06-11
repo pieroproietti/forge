7e689a6dfcaa70adedb0b57369ef69dbcc55f90d
Piero Proietti <piero.proietti@gmail.com>
2026-06-11 17:30:31 +0200

    uffa

--- linkforge.sh
+++ linkforge.sh
@@ -1,4 +1,4 @@
 ln -s ~/forge/oa-tools ~/oa-tools
-ln -s ~/forge/oa-wardrobe ~/oa-wardrobe
-ln -s ~/forge/penguins-blog ~/penguins-blog
-ln -s ~/forge/penguins-eggs ~/penguins-eggs
+#ln -s ~/forge/oa-wardrobe ~/oa-wardrobe
+#ln -s ~/forge/penguins-blog ~/penguins-blog
+#ln -s ~/forge/penguins-eggs ~/penguins-eggs
