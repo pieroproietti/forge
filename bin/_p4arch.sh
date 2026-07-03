9b55cba0a9c1e99a58a97a432a5fac73fbb306e2
Piero Proietti <piero.proietti@gmail.com>
2026-07-03 08:48:48 +0200

    uffissima

--- bin/_p4arch.sh
+++ bin/_p4arch.sh
@@ -4,7 +4,7 @@ sudo systemctl enable --now NetworkManager
 sudo nmcli con delete "ens18" 2>/dev/null || true
 sudo nmcli con add type ethernet ifname ens18 con-name ens18 \
   ipv4.method manual \
-  ipv4.addresses "$VM_ID_arch/24" \
+  ipv4.addresses "192.168.1.$VM_ID_arch/24" \
   ipv4.gateway 192.168.1.254 \
   ipv4.dns "192.168.1.254 8.8.8.8"
   
